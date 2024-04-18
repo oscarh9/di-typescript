4e83d1d388b9dc5e19a2e2940a0d5f220030e5b3
oscarh9 <oscarherenciasakkis@gmail.com>
2024-04-18 10:08:59 +0200

    completed exercises

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
 "use strict";
 class Book {
+    constructor(title) {
+        this.title = title;
+    }
+    read() {
+        return "Reading book";
+    }
 }
-class PaperBook {
+class PaperBook extends Book {
+    constructor(title, pages) {
+        super(title);
+        this.pages = pages;
+    }
+    read() {
+        return "Book in paper";
+    }
 }
-class ElectronicBook {
+class ElectronicBook extends Book {
+    constructor(title, size) {
+        super(title);
+        this.size = size;
+    }
+    read() {
+        return "Book in digital";
+    }
 }
-class ParchmentBook {
+class ParchmentBook extends Book {
+    constructor(title, large) {
+        super(title);
+        this.large = large;
+    }
+    read() {
+        return "Book in parchment";
+    }
 }
 class ReadBook {
+    constructor(books) {
+        this.books = books;
+    }
+    read() {
+        return this.books.read();
+    }
 }
 const phisicalBook = new PaperBook("Dragones de papel", 350);
 const digitalBook = new ElectronicBook("One piece", 150);
--- app.ts
+++ app.ts
@@ -1,12 +1,60 @@
-class Book{}
+class Book{
+    title: string;
+    constructor (title: string) {
+        this.title = title;
+    }
 
-class PaperBook{}
+    read () {
+        return "Reading book"
+    }
+}
 
-class ElectronicBook{}
+class PaperBook extends Book{
+    pages: number;
+    constructor (title: string, pages: number) {
+        super (title);
+        this.pages = pages;
+    }
 
-class ParchmentBook{}
+    read () {
+        return "Book in paper"
+    }
+}
 
-class ReadBook{}
+class ElectronicBook extends Book{
+    size: number;
+    constructor (title: string, size: number) {
+        super (title);
+        this.size = size;
+    }
+
+    read () {
+        return "Book in digital"
+    }
+}
+
+class ParchmentBook extends Book{
+    large: number;
+    constructor (title: string, large:number) {
+        super (title);
+        this.large = large;
+    }
+
+    read () {
+        return "Book in parchment"
+    }
+}
+
+class ReadBook{
+    books: Book;
+    constructor (books: Book) {
+        this.books = books;
+    }
+
+    read () {
+       return this.books.read()
+    }
+}
 
 
 const phisicalBook = new PaperBook("Dragones de papel",350);
